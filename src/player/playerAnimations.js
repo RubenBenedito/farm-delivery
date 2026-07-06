23f7cb6c3d0c423620063db554cb7a40763a0322
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-07-06 01:41:07 +0100

    Create playerAnimations.js

---
+++ src/player/playerAnimations.js
@@ -0,0 +1,35 @@
+export function createPlayerAnimations(scene) {
+    const anims = scene.anims;
+
+    // Baixo
+    anims.create({
+        key: 'walk-down',
+        frames: anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+    // Direita
+    anims.create({
+        key: 'walk-right',
+        frames: anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+    // Cima
+    anims.create({
+        key: 'walk-up',
+        frames: anims.generateFrameNumbers('player', { start: 8, end: 11 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+    // Esquerda
+    anims.create({
+        key: 'walk-left',
+        frames: anims.generateFrameNumbers('player', { start: 12, end: 15 }),
+        frameRate: 8,
+        repeat: -1
+    });
+}
