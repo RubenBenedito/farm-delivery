58489bc96f09f8101ec77e51a28fec01db43f0d4
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-07-10 00:22:01 +0100

    Atualização dos Produtos

--- src/items/products.js
+++ src/items/products.js
@@ -3,37 +3,59 @@ export const PRODUCTS = {
         id: "alface",
         image: "items/alface.png",
         defaultQuantity: 5,
-
-        // Preços dinâmicos
-        seedBasePrice: 1,
-        sellRange: [3, 5] 
+        buyPrice: 3,
+        sellRange: [3, 4]
     },
     cenoura: {
         id: "cenoura",
         image: "items/cenoura.png",
         defaultQuantity: 5,
-        seedBasePrice: 2,
-        sellRange: [5, 7] 
+        buyPrice: 4,
+        sellRange: [4, 6]
     },
     cebola: {
         id: "cebola",
         image: "items/cebola.png",
         defaultQuantity: 5,
-        seedBasePrice: 2,
-        sellRange: [5, 7]
+        buyPrice: 5,
+        sellRange: [4, 6]
     },
     pepino: {
         id: "pepino",
         image: "items/pepino.png",
         defaultQuantity: 5,
-        seedBasePrice: 2,
-        sellRange: [4, 6]
+        buyPrice: 4,
+        sellRange: [4, 5]
     },
     morango: {
         id: "morango",
         image: "items/morango.png",
         defaultQuantity: 5,
-        seedBasePrice: 3,
+        buyPrice: 6,
         sellRange: [8, 12]
+    },
+    batata: {
+        id: "batata",
+        image: "items/batata.png",
+        defaultQuantity: 0,
+        buyPrice: 5,
+        sellRange: [6, 9],
+        locked: true
+    },
+    couve: {
+        id: "couve",
+        image: "items/couve.png",
+        defaultQuantity: 0,
+        buyPrice: 5,
+        sellRange: [6, 9],
+        locked: true
+    },
+    melancia: {
+        id: "melancia",
+        image: "items/melancia.png",
+        defaultQuantity: 0,
+        buyPrice: 7,
+        sellRange: [10, 14],
+        locked: true
     }
 };
